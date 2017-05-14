b2fb7ee3eb9965618cab48f436788fdde93bff1d
amclees <andrewmclees5@gmail.com>
2017-05-13 21:21:46 -0700

    Add basic question listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { Question } from './question';
+
+function getQuestions(number) {
+  let questions = [];
+  for(let i = 0; i < number; i++) {
+    questions[i] = new Question;
+  }
+  return questions;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./main.css']
 })
+
 export class AppComponent {
   title = 'Hex Quiz';
+  questions = getQuestions(10);
+  getQuestions = getQuestions;
 }
